f1d57ae4dc08ec884ff5cdae3fe426b85c040f0b
Richard Dimalanta <chardmd.au@gmail.com>
2018-09-01 22:40:29 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# url-scrapper
+
+A node.js module that scraps the metadata of a URL
+
+### Install
+
+```
+npm install url-scrapper
+```
+
+### Usage
+
+```javascript
+import scrapper from 'url-scrapper'
+
+try {
+  const metadata = await scrapper("https://github.com")
+  console.log('metadata info', metadata);
+} catch (e) {
+  console.error(e);
+}
+```
+
+### License
+
+MIT
---
+++ scrapper/index.js
@@ -0,0 +1,58 @@
+const urlParser = require("url");
+const got = require("got");
+
+const metascraper = require("metascraper")([
+  require("metascraper-author")(),
+  require("metascraper-date")(),
+  require("metascraper-description")(),
+  require("metascraper-image")(),
+  require("metascraper-logo")(),
+  require("metascraper-clearbit-logo")(),
+  require("metascraper-publisher")(),
+  require("metascraper-title")(),
+  require("metascraper-url")(),
+  require("metascraper-logo-favicon")(),
+  require("metascraper-amazon")(),
+  require("metascraper-youtube")(),
+  require("metascraper-soundcloud")(),
+  require("metascraper-video-provider")()
+]);
+
+/**
+ * Get URL metadata
+ *
+ * @param {String} string
+ */
+
+async function scrapUrl(urlText) {
+  if (typeof urlText !== "string") {
+    return {
+      message: "URL is not a string"
+    };
+  }
+
+  const validUrl = urlParser.parse(urlText, true);
+  if (!validUrl) {
+    return {
+      message: "URL is not valid."
+    };
+  }
+
+  let metadata = null;
+  try {
+    const { body: html, url } = await got(urlText);
+    metadata = await metascraper({ html, url });
+  } catch (e) {
+    return {
+      message: `Failed scrapping data ${e}`
+    };
+  }
+
+  return metadata;
+}
+
+/**
+ * Expose `scrapUrl`.
+ */
+
+module.exports = scrapUrl;
